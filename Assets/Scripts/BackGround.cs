37619099cc6da738bab68f901542222f24971658
SAM <SAM@DESKTOP-UOTS38N>
2017-05-04 12:01:41 +1000

    My Assignment for programming

---
+++ Assets/Scripts/BackGround.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    public float scrollSpeed = 0.5f;
+    public Renderer rend;
+
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // moves the ground texture in a direction
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0)); 
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float movementSpeed = 0.0f;  //How fast we follow
+    public Transform target;            //Object we want to follow
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // enemy locks-on with player
+        transform.position = Vector3.MoveTowards(transform.position, target.position, movementSpeed);
+    }
+    void FixedUpdate()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.right);
+        Debug.DrawLine(transform.position, transform.position + fwd * 10f);
+        if (Physics.Raycast(transform.position, fwd, 10))
+            print("There is something in front of the object!");
+    }
+}
